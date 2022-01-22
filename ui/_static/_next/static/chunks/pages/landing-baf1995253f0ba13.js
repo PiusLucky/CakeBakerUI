7ec0052cb7d1879e2fb6ee4a95d075923716c6df
luckypius50 <luckypius50@gmail.com>
2022-01-22 10:59:44 +0100

    :art: Add static files

---
+++ ui/_static/_next/static/chunks/pages/landing-baf1995253f0ba13.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469],{9337:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/landing",function(){return u(1868)}])}},function(n){n.O(0,[37,868,774,888,179],(function(){return _=9337,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
